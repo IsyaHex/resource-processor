6aaddcb950d6f763cc1379f2da5588452b594048
IsyaHex <i.domlajanov@gmail.com>
2023-02-03 14:20:40 +0500

    Initial commit

---
+++ src/test/java/uz/epam/msa/processor/ResourceProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.epam.msa.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
