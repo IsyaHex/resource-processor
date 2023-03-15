e7255f980edd6ad21ac84cbbbcdca20dbbf5df62
IsyaHex <i.domlajanov@gmail.com>
2023-03-15 16:43:50 +0500

    Kafka settings updates

--- src/main/java/uz/epam/msa/processor/Constants.java
+++ src/main/java/uz/epam/msa/processor/Constants.java
@@ -9,7 +9,6 @@ public class Constants {
     public static final String SONG_SERVICE_URL = "http://localhost:1112/songs";
     public static final String SONG_DTO_FORMED_MSG = "SongDTO -> %s";
     public static final String RECEIVED_RESOURCE_ID = "Resource ID -> %s";
-    public static final String KAFKA_CONFIGURATION_URL = "127.0.0.1:9092";
     public static final String GET_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
 }
